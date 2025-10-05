2492fa9d3b10b4a7d784f4c8b23665f3349a33e3
Yashith_Prabhashwara <141606159+yasith-1@users.noreply.github.com>
2025-10-06 01:04:12 +0530

    create not found component and add it to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './home/home';
 import { Contact } from './contact/contact';
 import { About } from './about/about';
 import { Services } from './services/services';
+import { NotFound } from './not-found/not-found';
 
 // Add routes
 export const routes: Routes = [
@@ -26,5 +27,10 @@ export const routes: Routes = [
     {
         path: 'services',
         component: Services
+    },
+    // If not found matching load that
+    {
+        path: '**',
+        component: NotFound
     }
 ];
