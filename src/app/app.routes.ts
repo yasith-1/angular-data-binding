917b76a2585c063379ac82fddc46f7ea1c641319
Yashith_Prabhashwara <141606159+yasith-1@users.noreply.github.com>
2025-10-05 21:09:02 +0530

    feat : created home comp and add it to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+// Add routes
+export const routes: Routes = [
+    {
+        path: '/home',
+        component: Home
+    },
+    {
+        path: '/contact',
+        component: Contact
+    }
+];
